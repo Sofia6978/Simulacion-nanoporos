4383daf4e86d51bc3436ffc3370b456c4007fb48
Sofia6978 <sofia.myskovets22@gmail.com>
2025-06-25 10:44:23 +0200

    Delete Tutorial/5_current_dna directory

--- Tutorial/5_current_dna/addWater.tcl
+++
@@ -1,7 +0,0 @@
-# to use: vmd -dispdev text -e addWater.tcl
-
-package require solvate
-solvate poro+dna.psf poro+dna.pdb +z 20 -z 20 -o pore_solv
-file delete combine.psf combine.pdb
-mol delete top
-
